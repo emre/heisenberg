ea7d2b41ddba393ce3f5e84151e243298074db9e
Emre Yilmaz <mail@emreyilmaz.me>
2019-02-18 00:17:59 +0300

    Package the library

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='heisenberg',
+    version='0.0.1',
+    packages=find_packages(),
+    url='https://github.com/emre/heisenberg',
+    license='MIT',
+    author='Emre Yilmaz',
+    author_email='mail@emreyilmaz.me',
+    description='A Python library to interact with the drugwars online game'
+)
