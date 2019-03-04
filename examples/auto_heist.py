7275beb74c95ad1d4bae5158dd2682d9d1c84d18
Emre Yilmaz <mail@emreyilmaz.me>
2019-03-04 16:47:00 +0300

    auto heist should heist smarter by using the available balances

--- examples/auto_heist.py
+++ examples/auto_heist.py
@@ -1,17 +1,22 @@
 from heisenberg.core import Heisenberg
+from heisenberg.rest_client import RestClient
+from heisenberg.utils import calculate_balance
+
 import time
 
 
-PLAYER_USERNAME = "[username]"
-PLAYER_POSTING_KEY = "[private_posting_key]"
-INVEST_AMOUNT = 420
+PLAYER_USERNAME = "<player_acc>"
+PLAYER_POSTING_KEY = "<posting_key>"
 
 
 def main():
-    h = Heisenberg(PLAYER_USERNAME, PLAYER_POSTING_KEY)
+    heisenberg = Heisenberg(PLAYER_USERNAME, PLAYER_POSTING_KEY)
+    client = RestClient()
     while True:
-        tx_id, block_num = h.heist(INVEST_AMOUNT)
-        print(f"Invested {INVEST_AMOUNT} DRUGS. \n"
+        balance = calculate_balance('drug', client.info(PLAYER_USERNAME))
+        print(f"{balance} drugs are available on {PLAYER_USERNAME}")
+        tx_id, block_num = heisenberg.heist(int(balance * 0.99))
+        print(f"Invested {int(balance * 0.99)} DRUGS. \n"
               f"Transaction ID: {tx_id}, Block num: {block_num}")
         time.sleep(300)
 
